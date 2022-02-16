66ea80a92164564549176780389304a4b50e9e72
Term4K <pnplants@yahoo.com>
2022-02-16 14:14:20 -0500

    work done by end of class

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -1,3 +1,6 @@
+<?php
+require(__DIR__ . "/../../lib/functions.php");
+?>
 <form onsubmit="return validate(this)" method="POST">
     <div>
         <label for="email">Email</label>
@@ -24,8 +27,49 @@
 <?php
  //TODO 2: add PHP Code
 if(isset($_POST['email']) && isset($_POST['password']) && isset($_POST['confirm'])){
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $confirm = $_POST['confirm'];
+    $email = se($_POST, "email", "", false);
+    $password = se($_POST, "password", "", false);
+    $confirm = se($_POST, "confirm", "", false);
+
+    $hasError = false;
+    if(empty($email)) {
+        echo "Email must not be empty";
+        $hasError = true;
+    }
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid Email Address";
+        $hasError = true;
+    }
+    if(empty($password)) {
+        echo "password must not be empty";
+        $hasError = true;
+    }
+    if(empty($confirm)) {
+        echo "confirm password must not be empty";
+        $hasError = true;
+    }
+    if(strlen($password) < 8){
+        echo "Password too short";
+        $hasError = true;
+    }
+    if(strlen($password) > 0 && $password !== $confirm){
+        echo "Passwords must match";
+        $hasError = true;
+    }
+    if(!$hasError){
+        echo "Welcome, $email";
+
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+        $db = getDB();
+        $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password)");
+        try {
+            $stmt->execute([":email" => $email, ":password" => $hash]);
+            echo "Successfully registered!";
+        } catch (Exception $e) {
+            echo "There was a problem registering";
+            echo "<pre>" . var_export($e, true) . "</pre>";
+        }
+    }
 }
 ?>
