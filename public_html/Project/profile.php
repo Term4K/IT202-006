50e0c0236ebd05f55706923954f0ee588c4f2fcb
Term4K <pnplants@yahoo.com>
2022-05-11 23:35:37 -0400

    done

--- public_html/Project/profile.php
+++ public_html/Project/profile.php
@@ -1,11 +1,18 @@
 <?php
 require_once(__DIR__ . "/../../partials/nav.php");
 is_logged_in(true);
+
+$user_id = (int)se($_GET, "id", get_user_id(), false);
+$isMe = $user_id == get_user_id();
+$isEdit = isset($_GET["edit"]);
+
+$db = getDB();
 ?>
 <?php
-if (isset($_POST["save"])) {
+if (isset($_POST["save"]) && $isMe && $isEdit) {
     $email = se($_POST, "email", null, false);
     $username = se($_POST, "username", null, false);
+    $vis = isset($_POST["vis"]) ? 1 : 0;
     $hasError = false;
     //sanitize
     $email = sanitize_email($email);
@@ -19,9 +26,8 @@ if (isset($_POST["save"])) {
         $hasError = true;
     }
     if (!$hasError) {
-        $params = [":email" => $email, ":username" => $username, ":id" => get_user_id()];
-        $db = getDB();
-        $stmt = $db->prepare("UPDATE Users set email = :email, username = :username where id = :id");
+        $params = [":email" => $email, ":username" => $username, ":id" => get_user_id(), ":vis" => $vis];
+        $stmt = $db->prepare("UPDATE Users set email = :email, username = :username, visibility = :vis where id = :id");
         try {
             $stmt->execute($params);
             flash("Profile saved", "success");
@@ -87,14 +93,122 @@ if (isset($_POST["save"])) {
         }
     }
 }
+
+$stmt = $db->prepare("SELECT id, email, username,visibility, created from Users where id = :id LIMIT 1");
+$isVisible = false;
+try {
+    $stmt->execute([":id" => $user_id]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($user) {
+        if ($isMe) {
+            $_SESSION["user"]["email"] = $user["email"];
+            $_SESSION["user"]["username"] = $user["username"];
+        }
+        if (se($user, "visibility", 0, false) > 0) {
+
+            $isVisible = true;
+        }
+        $email = se($user, "email", "", false);
+        $username = se($user, "username", "", false);
+        $joined = se($user, "created", "", false);
+    } else {
+        flash("User doesn't exist", "danger");
+    }
+} catch (Exception $e) {
+    flash("An unexpected error occurred, please try again", "danger");
+    //echo "<pre>" . var_export($e->errorInfo, true) . "</pre>";
+}
 ?>
 
 <?php
 $email = get_user_email();
-$username = get_username();
-refresh_credit_balance(get_user_id());
+if($isMe){
+    $username = get_username();
+    refresh_credit_balance(get_user_id());
+}
 ?>
 <div class="container-fluid">
+    <h1>Profile</h1>
+
+    <?php if ($isMe && $isEdit) : ?>
+        <?php if ($isMe) : ?>
+            <h4><a href="<?php echo get_url("profile.php"); ?>">View Public Profile</a></h4>
+        <?php endif; ?>
+        <h3>Credits: <?php echo get_credits();?></h3>
+        <div>
+            Best Score: <?php echo get_best_score(get_user_id()); ?>
+        </div>
+        <div>
+            <?php
+            $duration = "latest";
+            require(__DIR__ . "/../../partials/scores_table.php");
+            ?>
+        </div>
+        <form method="POST" onsubmit="return validate(this);">
+            <div class="mb-2">
+                <label class="form-label" for="email">Email</label>
+                <input class="form-control" type="email" name="email" id="email" value="<?php se($email); ?>" />
+            </div>
+            <div class="mb-2">
+                <label class="form-label" for="username">Username</label>
+                <input class="form-control" type="text" name="username" id="username" value="<?php se($username); ?>" />
+            </div>
+            <div class="mb-3">
+                <div class="form-check form-switch">
+                    <input <?php if ($isVisible) {
+                                echo "checked";
+                            } ?> class="form-check-input" type="checkbox" role="switch" id="vis" name="vis">
+                    <label class="form-check-label" for="vis">Toggle Visibility</label>
+                </div>
+            </div>
+            <!-- DO NOT PRELOAD PASSWORD -->
+            <div class="mb-2">Password Reset</div>
+            <div class="mb-2">
+                <label class="form-label" for="cp">Current Password</label>
+                <input class="form-control" type="password" name="currentPassword" id="cp" />
+            </div>
+            <div class="mb-2">
+                <label class="form-label" for="np">New Password</label>
+                <input class="form-control" type="password" name="newPassword" id="np" />
+            </div>
+            <div class="mb-2">
+                <label class="form-label" for="conp">Confirm Password</label>
+                <input class="form-control" type="password" name="confirmPassword" id="conp" />
+            </div>
+            <input type="submit" class="mt-3 btn btn-primary" value="Update Profile" name="save" />
+        </form>
+    <?php else : ?>
+        <?php if ($isMe) : ?>
+            <a href="?edit">Edit</a>
+        <?php endif; ?>
+        <?php if ($isVisible || $isMe) : ?>
+
+            <h2>This is <?php se($username); ?></h2>
+            <div>
+               <h4> Best Score: <?php echo get_best_score($user_id); ?></h4>
+            </div>
+            <div>
+                <h4>Joined: <?php se($joined); ?> </h4>
+            </div>
+            <div>
+                <?php
+                $duration = "latest";
+                //Note: $user_id will be defined prior to this require() so should use whatever is set at the top
+                require(__DIR__ . "/../../partials/scores_table.php");
+                ?>
+            </div>
+        <?php else : ?>
+            Profile is private
+            <?php
+            flash("Profile is private", "warning");
+            redirect("home.php");
+            ?>
+        <?php endif; ?>
+</div>
+<?php endif; ?>
+
+
+<!-- <div class="container-fluid">
     <h1>Profile</h1>
     <h3>Credits: <?php echo get_credits();?></h3>
     <div>
@@ -115,7 +229,7 @@ refresh_credit_balance(get_user_id());
             <label class="form-label" for="username">Username</label>
             <input class="form-control" type="text" name="username" id="username" value="<?php se($username); ?>" />
         </div>
-        <!-- DO NOT PRELOAD PASSWORD -->
+
         <div class="mb-2">Password Reset</div>
         <div class="mb-2">
             <label class="form-label" for="cp">Current Password</label>
@@ -131,7 +245,7 @@ refresh_credit_balance(get_user_id());
         </div>
         <input type="submit" class="mt-3 btn btn-primary" value="Update Profile" name="save" />
     </form>
-</div>
+</div> -->
 
 
 <script>
